d21572a3cf6d305c675729a0b0d423a6242e0972
catherine <vuthi.esther@gmail.com>
2022-06-20 13:48:51 +0300

    commit tree 0f4f77e6d400aaa3025c03428e1c3c6a8797230d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
+  #----------------- GET Request ----------------------#
+
   get '/games' do
     games = Game.all.order(:title).limit(10)
     games.to_json
   end
+
 
   get '/games/:id' do
     game = Game.find(params[:id])
@@ -14,6 +17,37 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  #----------------- POST Request ----------------------#
+
+  post '/reviews' do
+  review = Review.create(
+    score: params[:score],
+    comment: params[:comment],
+    game_id: params[:game_id],
+    user_id: params[:user_id]
+  )
+  review.to_json
+end
+
+  #----------------- PATCH Request ----------------------#
+  
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
+  #----------------- DELETE Request ----------------------#
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
   end
 
 end
